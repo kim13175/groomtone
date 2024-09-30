1e43d037fdcb2a30464148002b9c318eabca8c31
beomjo <dongjun1315@naver.com>
2024-09-30 15:24:41 +0900

    spreadsheet with java final feature implement

---
+++ spread_sheet/script.js
@@ -0,0 +1,143 @@
+const spreadSheetContainer = document.querySelector('#spreadsheet-container');
+const exportBtn = document.querySelector('#export-btn');
+const ROWS = 10;
+const COLS = 10;
+const spreadsheet = [];
+const alphabet = [
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" 
+];
+
+class Cell{
+    constructor(isHeader, disabled, data, row, column, rowName, columnName, active = false){
+        this.isHeader = isHeader;
+        this.disabled = disabled;
+        this.data = data;
+        this.row = row;
+        this.column = column;
+        this.rowName = rowName;
+        this.columnName = columnName;
+        this.active = active;
+    }
+}
+
+exportBtn.onclick = function(e){
+    let csv = "";
+    for (let i = 0; i < spreadsheet.length; i++){
+        if(i === 0) continue;
+
+        csv += spreadsheet[i]
+        .filter(item => !item.isHeader)
+        .map(item => item.data)
+        .join(',') + "\r\n";
+    }
+    const csvObj = new Blob([csv]);
+    const csvUrl = URL.createObjectURL(csvObj); 
+    console.log('csvUrl', csvUrl);
+
+    const a = document.createElement("a");
+    a.href = csvUrl;
+    a.download = 'spreadsheet name.csv';
+    a.click();
+}
+
+initSpreadsheet();
+
+function initSpreadsheet(){
+    for (let i = 0; i < ROWS; i++){
+        let spreadsheetRow = [];
+        for (let j = 0; j < COLS; j++){
+            let cellData = "";
+            let isHeader = false;
+            let disabled = false;
+
+            // 각 행의 첫번째열에 숫자 넣기
+            if (j === 0){
+                cellData = i;
+                isHeader = true;
+                disabled = true;
+            }
+            
+            if (i === 0){
+                cellData = alphabet[j - 1];
+                isHeader = true;
+                disabled = true;
+            }
+            
+            if (!cellData){
+                cellData = "";
+            }
+
+            // 첫 번째 행의 열은 공백처리
+            if (cellData <= 0){
+                cellData = "";
+            }
+
+            const rowName = i;
+            const columnName = alphabet[j - 1];
+
+            const cell = new Cell(isHeader, disabled, cellData, i, j, rowName, columnName, false);
+            spreadsheetRow.push(cell);
+        }
+        spreadsheet.push(spreadsheetRow);
+    }
+    console.log(spreadsheet);
+}
+
+function createCellElement(cell){
+    const cellEl = document.createElement('input');
+    cellEl.className = 'cell';
+    cellEl.id = 'cell_' + cell.row + cell.column;
+    cellEl.value = cell.data;
+    cellEl.disabled = cell.disabled;
+
+    if (cell.isHeader){
+        cellEl.classList.add('header');
+    }
+
+    cellEl.onclick = () => handleCellClick(cell);
+    cellEl.onchange = (e) => handleOnChange(e.target.value, cell);
+
+    return cellEl;
+}
+
+function handleOnChange(data, cell){
+    cell.data = data;
+}
+
+function handleCellClick(cell){
+    clearHeaderActiveStates();
+    const columnHeader = spreadsheet[0][cell.column];
+    const rowHeader = spreadsheet[cell.row][0];
+    const columnHeaderEl = getElementFromRC(columnHeader.row, columnHeader.column);
+    const rowHeaderEl = getElementFromRC(rowHeader.row, rowHeader.column);
+    columnHeaderEl.classList.add('active');
+    rowHeaderEl.classList.add('active');
+    document.querySelector('#cell-status').innerHTML = cell.columnName + cell.rowName;
+}
+
+function clearHeaderActiveStates(){
+    const headers = document.querySelectorAll('.header');
+
+    headers.forEach((header) => {
+        header.classList.remove('active');
+    });
+}
+
+function getElementFromRC(row, col){
+    return document.querySelector("#cell_" + row + col);
+}
+
+// html에 10 * 10 의 셀을 렌더링
+function drawSheet(){
+    for (let i = 0; i < spreadsheet.length; i++){
+        const rowContainerEl = document.createElement("div");
+        rowContainerEl.className = "cell-row";
+        for (let j = 0; j < spreadsheet[i].length; j++){
+            const cell = spreadsheet[i][j];
+            spreadSheetContainer.append(createCellElement(cell));
+        }
+        spreadSheetContainer.append(rowContainerEl);
+    }
+}
+
+drawSheet();
